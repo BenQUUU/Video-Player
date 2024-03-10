fb68248155a74c2525871e1b120da3c9d3a68c30
Michał Kolczak <michal.kolczak123@gmail.com>
2024-03-10 12:06:11 +0100

    a new design of the app was created

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,63 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    ui->pushButton_Play_Pause->setIcon(style()->standardIcon(QStyle::SP_MediaPlay));
+    ui->pushButton_Stop->setIcon(style()->standardIcon(QStyle::SP_MediaStop));
+    ui->pushButton_Seek_Backward->setIcon(style()->standardIcon(QStyle::SP_MediaSeekBackward));
+    ui->pushButton_Seek_Forward->setIcon(style()->standardIcon(QStyle::SP_MediaSeekForward));
+    ui->pushButton_Volume->setIcon(style()->standardIcon(QStyle::SP_MediaVolume));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_actionOpen_triggered()
+{
+
+}
+
+
+void MainWindow::on_horizontalSlider_Duration_valueChanged(int value)
+{
+
+}
+
+
+void MainWindow::on_pushButton_Seek_Backward_toggled(bool checked)
+{
+
+}
+
+
+void MainWindow::on_pushButton_Seek_Forward_toggled(bool checked)
+{
+
+}
+
+
+void MainWindow::on_pushButton_Play_Pause_clicked()
+{
+
+}
+
+
+void MainWindow::on_pushButton_Stop_clicked()
+{
+
+}
+
+
+void MainWindow::on_pushButton_Volume_clicked()
+{
+
+}
+
+
+void MainWindow::on_horizontalSlider_Volume_valueChanged(int value)
+{
+
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QtMultimedia>
+#include <QtMultimediaWidgets>
+#include <QtCore>
+#include <QtWidgets>
+#include <QtGui>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +22,29 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionOpen_triggered();
+
+    void on_horizontalSlider_Duration_valueChanged(int value);
+
+    void on_pushButton_Seek_Backward_toggled(bool checked);
+
+    void on_pushButton_Seek_Forward_toggled(bool checked);
+
+    void on_pushButton_Play_Pause_clicked();
+
+    void on_pushButton_Stop_clicked();
+
+    void on_pushButton_Volume_clicked();
+
+    void on_horizontalSlider_Volume_valueChanged(int value);
+
 private:
     Ui::MainWindow *ui;
+    QMediaPlayer *Player;
+    QVideoWidget *Video;
+    qint64 mDuration;
+    bool IS_Pause = true;
+    bool IS_Muted = false;
 };
 #endif // MAINWINDOW_H
