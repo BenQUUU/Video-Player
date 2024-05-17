014aa5842cac020a0b5ddb9f96067efd5bd704fe
Michał Kolczak <michal.kolczak123@gmail.com>
2024-05-17 11:55:56 +0200

    fixed the issue of missing audio after video processing

--- videoprocessor.cpp
+++ videoprocessor.cpp
@@ -5,6 +5,84 @@
  */
 VideoProcessor::VideoProcessor() {}
 
+double VideoProcessor::getVideoDuration(const QString& fileName) {
+    QString program = "/usr/bin/ffprobe";
+    QStringList arguments;
+    arguments << "-v" << "error"
+              << "-show_entries" << "format=duration"
+              << "-of" << "default=noprint_wrappers=1:nokey=1"
+              << fileName;
+
+    QProcess process;
+    process.start(program, arguments);
+    if (!process.waitForStarted()) {
+        qDebug() << "Failed to start FFprobe process";
+        return -1;
+    }
+
+    process.waitForFinished(-1);
+
+    QString output = process.readAllStandardOutput();
+    bool ok;
+    double duration = output.trimmed().toDouble(&ok);
+    if (!ok) {
+        qDebug() << "Failed to parse video duration";
+        return -1;
+    }
+
+    return duration;
+}
+
+void VideoProcessor::mergeAudioVideo_(const QString& inputFileName, const QString& outputFileName) {
+    QString tempOutputFileName = outputFileName + ".tmp.mp4";
+
+    double duration = getVideoDuration(outputFileName);
+    if (duration < 0) {
+        QMessageBox::critical(nullptr, "Error", "Failed to get video duration.");
+        return;
+    }
+
+    QString program = "/usr/bin/ffmpeg";
+    QStringList arguments;
+    arguments << "-y"
+              << "-i" << inputFileName
+              << "-i" << outputFileName
+              << "-t" << QString::number(duration)
+              << "-map" << "0:a"
+              << "-map" << "1:v"
+              << "-c:v" << "libx264"
+              << "-c:a" << "aac"
+              << "-strict" << "experimental"
+              << "-shortest"
+              << "-async" << "1"
+              << tempOutputFileName;
+
+    qDebug() << "Running FFmpeg with arguments:" << arguments;
+
+    QProcess process;
+    process.start(program, arguments);
+    if (!process.waitForStarted()) {
+        qDebug() << "Failed to start FFmpeg process";
+        return;
+    }
+
+    process.waitForFinished(-1);
+
+    QString ffmpegOutput = process.readAllStandardOutput();
+    QString ffmpegError = process.readAllStandardError();
+
+    qDebug() << "FFmpeg output:" << ffmpegOutput;
+    qDebug() << "FFmpeg error:" << ffmpegError;
+
+    if (process.exitStatus() != QProcess::NormalExit || process.exitCode() != 0) {
+        qDebug() << "FFmpeg command:" << program << arguments.join(" ");
+        QMessageBox::critical(nullptr, "Error", "Failed to merge audio and video. FFmpeg error: " + ffmpegError);
+        return;
+    }
+
+    QFile::remove(outputFileName);
+    QFile::rename(tempOutputFileName, outputFileName);
+}
 
 /**
  * @brief Performs human detection on a video file.
@@ -46,6 +124,8 @@ void VideoProcessor::processHumanDetection(const QString& inputFileName, const Q
 
     cap.release();
     out.release();
+
+    mergeAudioVideo_(inputFileName, outputFileName);
 
     QMessageBox::information(nullptr, "Info", "Human detection completed. Saved as: " + outputFileName);
 }
@@ -97,6 +177,8 @@ void VideoProcessor::processMotionDetection(const QString& inputFileName, const 
 
     cap.release();
     out.release();
+
+    mergeAudioVideo_(inputFileName, outputFileName);
 
     QMessageBox::information(nullptr, "Info", "Motion detection completed. Saved as: " + outputFileName);
 }
